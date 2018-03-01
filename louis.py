426628291349f0c18990e1e9332d3404c259bb62
LouisDumont <33422071+LouisDumont@users.noreply.github.com>
2018-03-01 20:27:11 +0100

    1e fonction score

--- louis.py
+++ louis.py
@@ -3,9 +3,17 @@
 from core import *
 
 def lenght(ride):
-    return (abs(ride.x0-ride.x1) + abs(ride.y0-ride.y1))
+    return (dist(ride.start,ride.end))
 
 def ratioScore(B,t,car,ride):
-    baseBenef = lenght(ride)
+    timeToStart = dis(car.pos,ride.start)
+    hereInTime = ((ride.tf - t) > (timeToStart+dis(ride.start,ride.end)))
+    score = -1
+    if hereInTime:
+        hereForBonus = ((ride.ti - t)>tAtteinte)
+        totBenef = lenght(ride) + hereForBonus*B
+        time = [max(ti,t+tAtteinte) + lenght(ride)] - t
+        score = totBenf ./ time
+    return score 
     
 
