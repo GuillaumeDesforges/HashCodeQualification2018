89ee53116c81fce874e9de6874639be253839c9b
Guillaume Desforges <aceus02@gmail.com>
2018-03-01 19:54:24 +0100

    Fix read

--- core.py
+++ core.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 class Ride:
     def __init__(self, x0, y0, x1, y1, ti, tf):
         self.x0 = x0
@@ -8,7 +10,16 @@ class Ride:
         self.tf = tf
 
     def __str__(self):
-        return ' '.join([x0, y0, x1, y1, ti, tf])
+        return ' '.join(map(str, [x0, y0, x1, y1, ti, tf]))
+
+class Car:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.target = None
+
+    def distance(self, x, y):
+        return abs(self.x - x) + abs(self.y - y)
 
 def read(file_path):
     with open(file_path) as f:
@@ -16,9 +27,13 @@ def read(file_path):
         R, C, F, N, B, T = tuple(map(int, lines[0].split(" ")))
         rides = []
         for line in lines[1:]:
-            ride = Ride(*tuple(map(int, lines[0].split(" "))))
+            ride = Ride(*tuple(map(int, line.split(" "))))
             rides.append(ride)
         return R, C, F, N, B, T, rides
-            
+
+# Example
+def fscore0(B, t, car, ride):
+    return 1
+
 if __name__ == "__main__":
     print(*read("a_example.in"))
---
+++ glouton.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from core import *
+
+def glouton(R, C, F, N, B, T, rides, fscore):
+    cars = [Car() for i in range(F)]
+    for t in range(T):
+        for car in cars:
+            if car.target is not None:
+               pass 
+
+if __name__ == "__main__":
+    R, C, F, N, B, T, rides = 
+    glouton(*read("a_example.in"), fscore0)
