4bede5678cae072ad8ba66020796d688f82846dc
LouisDumont <33422071+LouisDumont@users.noreply.github.com>
2018-03-01 21:01:54 +0100

    Fonction score fonctionnelle

--- louis.py
+++ louis.py
@@ -4,16 +4,24 @@ from core import *
 
 def lenght(ride):
     return (dist(ride.start,ride.end))
-
-def ratioScore(B,t,car,ride):
-    timeToStart = dis(car.pos,ride.start)
-    hereInTime = ((ride.tf - t) > (timeToStart+dis(ride.start,ride.end)))
-    score = -1
-    if hereInTime:
-        hereForBonus = ((ride.ti - t)>tAtteinte)
-        totBenef = lenght(ride) + hereForBonus*B
-        time = [max(ti,t+tAtteinte) + lenght(ride)] - t
-        score = totBenf ./ time
-    return score 
     
 
+def ratioScore(B,t,car,ride):
+    timeToStart = dist(car.pos,ride.start)
+    hereInTime = ((ride.tf - t) > (timeToStart+dist(ride.start,ride.end)))
+    score = -1
+    if hereInTime:
+        hereForBonus = ((ride.ti - t)>timeToStart)
+        totBenef = lenght(ride)
+        if hereForBonus:
+            totBenef += B
+        time = (max(ride.ti,t+timeToStart) + lenght(ride)) - t
+        score = totBenef / time
+    return score
+
+    
+# if __name__==__main__:
+#ride = Ride(1,1,5,5,1,20)
+#car = Car()
+#print(ratioScore(2,0,car,ride))
+#print(dist(ride.start,ride.end))
