a91fc5ad677a026520efbcb930c3bf90fd93f2b6
LouisDumont <33422071+LouisDumont@users.noreply.github.com>
2018-03-01 21:47:29 +0100

    Map

--- louis.py
+++ louis.py
@@ -1,6 +1,7 @@
 """Fonctions Score"""
 
 from core import *
+import numpy as np
 
 def lenght(ride):
     return (dist(ride.start,ride.end))
@@ -18,7 +19,27 @@ def ratioScore(B,t,car,ride):
         time = (max(ride.ti,t+timeToStart) + lenght(ride)) - t
         score = totBenef / time
     return score
+    
+def eval_density(R,C,cars):
+    mappe = np.ones((R//10,C//10))
+    for car in cars:
+        mappe[car.pos.x//10,car.pos.y//10] += 1
+    return(mappe)
 
+
+
+def evolutedScore(B,t,car,ride):
+    timeToStart = dist(car.pos,ride.start)
+    hereInTime = ((ride.tf - t) > (timeToStart+dist(ride.start,ride.end)))
+    score = -1
+    if hereInTime:
+        hereForBonu,s = ((ride.ti - t)>timeToStart)
+        totBenef = lenght(ride)
+        if hereForBonus:
+            totBenef += B
+        time = (max(ride.ti,t+timeToStart) + lenght(ride)) - t
+        score = totBenef / time
+    return score
     
 # if __name__==__main__:
 #ride = Ride(1,1,5,5,1,20)
